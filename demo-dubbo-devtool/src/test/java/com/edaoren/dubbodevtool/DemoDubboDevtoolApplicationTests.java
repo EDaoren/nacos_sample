d6dc25830bd7cf1012a42b8d66177a88b6dcc2f2
EDaoren <550790375@qq.com>
2021-12-26 15:28:22 +0800

    调整项目结构

---
+++ demo-dubbo-devtool/src/test/java/com/edaoren/dubbodevtool/DemoDubboDevtoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edaoren.dubbodevtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoDubboDevtoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ demo-export/dubbo-service-api/src/main/java/com/edaoren/api/dto/ExportParamDTO.java
@@ -0,0 +1,13 @@
+package com.edaoren.api.dto;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * @author EDaoren
+ */
+@Data
+public class ExportParamDTO implements Serializable {
+
+}
